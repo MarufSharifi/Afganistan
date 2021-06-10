b98aec63d33e658e128bde504dfad12849a09b64
maruf <marufsharifi97103@gmail.com>
2021-06-10 21:41:48 +0430

    added react-native-screens

--- src/routes/mainNavigation.tsx
+++ src/routes/mainNavigation.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import Home from '../screen/homePage';
 import NavigationDrawer from '../routes/navigationDrawer';
 import Province from '../screen/Province';
 import Description from '../screen/districtDescription';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+
+// Documentation: https://reactnavigation.org/docs/stack-navigator/
+let Stack = createNativeStackNavigator();
+
+if (Platform.OS === 'ios') {
+  let iosVersion = parseFloat(Platform.Version)?.toFixed();
+  if (iosVersion < 13) {
+    Stack = createStackNavigator();
+    // if there is a bug in finding the os version
+    // then also use the traditional stack
+  } else if (!iosVersion) {
+    Stack = createStackNavigator();
+  }
+}
+
 const MainNavigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      screenOptions={{
+        headerTopInsetEnabled: false,
+        direction: 'rtl',
+        gestureEnabled: true,
+      }}>
       <Stack.Navigator>
         <Stack.Screen
           name="homePage"
